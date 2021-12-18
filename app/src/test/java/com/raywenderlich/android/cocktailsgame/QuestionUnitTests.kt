25b5d2b5f19c09c5eba0a1069ddfe01f0cc71fd1
Nadir Hussain <nadirhussain1@gmail.com>
2021-12-18 09:40:19 +0500

    - Refactored Question class unit tests by moving common line into setUp method.

--- app/src/test/java/com/raywenderlich/android/cocktailsgame/QuestionUnitTests.kt
+++ app/src/test/java/com/raywenderlich/android/cocktailsgame/QuestionUnitTests.kt
@@ -1,44 +1,43 @@
 package com.raywenderlich.android.cocktailsgame
 
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 
 class QuestionUnitTests {
 
+    lateinit var question:Question
+
+    @Before
+    fun setUp(){
+        question = Question("Correct","Incorrect")
+    }
+
     @Test
     fun whenCreatingQuestion_shouldNotHaveAnsweredOption(){
-        val question = Question("Correct","Incorrect")
         Assert.assertNull(question.answeredOption)
     }
 
     @Test
     fun whenAnswering_shouldHaveAnsweredOption(){
-        val question = Question("Correct","Incorrect")
         question.answer("Incorrect")
-
         Assert.assertEquals("Incorrect",question.answeredOption)
     }
 
     @Test
     fun whenAnswering_withCorrectOption_shouldReturnTrue(){
-        val question = Question("Correct","Incorrect")
         val result = question.answer("Correct")
-
         Assert.assertTrue(result)
     }
 
     @Test
     fun whenAnswering_withIncorrectOption_shouldReturnFalse(){
-        val question=Question("Correct","Incorrect")
         val result = question.answer("Incorrect")
-
         Assert.assertFalse(result)
     }
 
     @Test(expected = IllegalArgumentException::class)
     fun whenAnswering_withInvalidOption_shouldThrowException(){
-        val question=Question("Correct","Incorrect")
-
         question.answer("Invalid")
     }
 
